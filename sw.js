bcdc7ba39811c09e43eec0243fae6b6d95023011
fulanite <matexcardozo@gmail.com>
2026-03-07 22:58:14 -0300

    Update SW version to 43.51 to force PWA refresh

--- sw.js
+++ sw.js
@@ -1,9 +1,9 @@
 /**
  * SGND - Service Worker for PWA
- * Version: 43.41
+ * Version: 43.51
  */
 
-const CACHE_NAME = 'sgnd-cache-v133';
+const CACHE_NAME = 'sgnd-cache-v134';
 const OFFLINE_URL = '/offline.html';
 
 // Assets to cache
