793991a355a63bf1fcba1296c178106e5f438421
fulanite <matexcardozo@gmail.com>
2026-03-24 23:29:04 -0300

    feat(ui): adapt Asignaciones and Devoluciones for mobile view

--- sw.js
+++ sw.js
@@ -1,9 +1,9 @@
 /**
  * SGND - Service Worker for PWA
- * Version: 43.63
+ * Version: 43.64
  */
 
-const CACHE_NAME = 'sgnd-cache-v145';
+const CACHE_NAME = 'sgnd-cache-v146';
 const OFFLINE_URL = '/offline.html';
 
 // Assets to cache
